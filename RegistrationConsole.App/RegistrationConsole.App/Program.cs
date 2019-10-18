908be5d46da15340b304387d1a78699a678d152a
BojidarSamokovski <fmi@FMI-536-1>
2019-10-18 14:14:35 +0300

    Initial login/register flow

--- RegistrationConsole.App/RegistrationConsole.App/Program.cs
+++ RegistrationConsole.App/RegistrationConsole.App/Program.cs
@@ -10,14 +10,55 @@ namespace RegistrationConsole.App
 	{
 		static void Main(string[] args)
 		{
-			List<string> listNames	= new List<string>()
+			Console.WriteLine("Press 1 for registration or 2 for login:");
+			string input = Console.ReadLine();
+			switch (input)
+			{
+				case "1":
+					RegisterUser();
+					break;
+				case "2":
+					LoginUser();
+					break;
+				default:
+					Console.WriteLine("GO F YOURSELF");
+					break;
+			}
+
+			//Console.WriteLine("Username:");
+			//string username = Console.ReadLine();
+
+			//Console.WriteLine("Password:");
+			//string password = Console.ReadLine();
+
+			//if (password.Length<3)
+			//{
+			//	Console.WriteLine($"password must be atleast 3 chars long.");
+			//}
+			//else
+			//{
+			//	Console.WriteLine($"username:{username}");
+			//	Console.WriteLine($"password:{password}");
+			//}
+			//Insert data in DB
+
+		}
+
+		private static void LoginUser()
+		{
+			List<string> listNames = new List<string>()
 			{
 				"Boiko Borisov",
 				"Amazon Prime",
 				"Sinyoto Momche"
 			};
 
-			Console.WriteLine( String.Join(" ", listNames ) );
+			Console.WriteLine(String.Join(" ", listNames));
+		}
+
+		private static void RegisterUser()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
